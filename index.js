0b9bffb5aed7fc9496c1cea60ec89650817398b1
Tony Zhao <41414030+tawnyzhao@users.noreply.github.com>
2020-03-02 04:40:40 -0500

    Refactor initial page load

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
 const puppeteer = require('puppeteer');
 const config = require('./config.json');
 
-(async () => {
-    const browser = await puppeteer.launch(
-        {headless: false}
-    );
+const reload = async (page, text) => {
+    await page.reload();
+    const rankings = await page.$("#mainContentDiv > div.orbisTabContainer > div.tab-content");
+    const currentText = await page.evaluate(rankings => rankings.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim(), rankings);
+    if (currentText !== text) {
+        console.log("Rankings are out!");
+        console.log(text);
+        await page.screenshot({ path: 'screenshot.png' });
+    }
+    console.log("Logged at: " + new Date().toJSON().slice(0, 19).replace(/[-T]/g, ':'));
+}
 
-    const page = await browser.newPage();
+const initialNavigation = async (page) => {
     await page.goto('https://waterlooworks.uwaterloo.ca/home.htm');
     await page.click("a.btn--landing");
     await page.waitForNavigation();
-
     await page.type('#userNameInput', config.email);
     await page.click("#nextButton");
     await page.type("#passwordInput", config.password);
     await page.click("#submitButton");
     await page.waitForSelector("#displayStudentMyRankings > a");
     await page.click("#displayStudentMyRankings > a");
+}
+
+(async () => {
+    const browser = await puppeteer.launch(
+        {headless: false}
+    );
+
+    const page = await browser.newPage();
+    await initialNavigation(page);
+
     await page.waitForSelector("#mainContentDiv > div.orbisTabContainer > div.tab-content");
     const rankings = await page.$("#mainContentDiv > div.orbisTabContainer > div.tab-content");
     const text = await page.evaluate(rankings => rankings.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim(), rankings);
-    console.log(text);
     await page.screenshot({ path: 'screenshot.png' });
+
+    setInterval((page, text) => reload(page, text), 10000, page, text);
 })();

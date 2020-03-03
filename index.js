6a216ff43ee9ce29bb2756610992b83368a5c55e
Tony Zhao <41414030+tawnyzhao@users.noreply.github.com>
2020-03-03 14:59:18 -0500

    Add full page screen shotting and debugging

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const puppeteer = require('puppeteer');
 const mailer = require('./mailer.js');
 const config = require('./config.json');
+const DEBUG = process.argv.includes('--dev');
 
 const reload = async (page, text) => {
     await page.reload();
     const rankings = await page.$("#mainContentDiv > div.orbisTabContainer > div.tab-content");
     const currentText = await page.evaluate(rankings => rankings.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim(), rankings);
-    if (currentText !== text) {
+    if (currentText === text) {
         console.log("Rankings are out!");
-        console.log(text);
-        await page.screenshot({ path: 'screenshot.png' });
-        await mailer.sendEmail(text, 'screenshot.png');
+        if (DEBUG) {
+            console.log(text);
+            console.log("------------");
+            console.log(currentText);
+        }
+        await page.screenshot({ path: 'screenshot.png', fullPage: true });
+        await mailer.sendEmail(text, 'screenshot.png')
+        process.exit(0);        
     }
     console.log("Checked at: " + new Date().toJSON().slice(0, 19).replace(/[-T]/g, ':'));
 }
@@ -29,7 +35,7 @@ const initialNavigation = async (page) => {
 
 (async () => {
     const browser = await puppeteer.launch(
-        { headless: true }
+        { headless: !DEBUG }
     );
 
     const page = await browser.newPage();
@@ -39,6 +45,6 @@ const initialNavigation = async (page) => {
     const rankings = await page.$("#mainContentDiv > div.orbisTabContainer > div.tab-content");
     const text = await page.evaluate(rankings => rankings.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim(), rankings);
     await page.screenshot({ path: 'screenshot.png' });
-
-    setInterval((page, text) => reload(page, text), 10000, page, text);
+    await reload(page,text);
+    setInterval((page, text) => reload(page, text), config.delay, page, text);
 })();
